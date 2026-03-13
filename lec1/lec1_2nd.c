b4894df00fdf1360ea0a9906b4b912660c26d77e
taekyulee <53734093+tklee-yonsei@users.noreply.github.com>
2026-03-13 05:16:34 +0000

    Add second C program for basic arithmetic operations

---
+++ lec1/lec1_2nd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("First number: ");
+    scanf("%d", &num1);
+    printf("Second number: ");
+    scanf("%d", &num2);
+
+    printf("Inputted numbers: %d, %d\n", num1, num2);
+    printf("----------\n");
+    printf("Sum: %d\n", num1 + num2);
+    printf("Difference: %d\n", num1 - num2);
+    printf("Product: %d\n", num1 * num2);
+    return 0;
+}
