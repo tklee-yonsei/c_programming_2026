fa34a5567a8aeb276df519a0566110c0510b7355
taekyulee <53734093+tklee-yonsei@users.noreply.github.com>
2026-03-13 05:05:07 +0000

    Add first and second C programs for demonstration

---
+++ lec1/lec1_1st.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 65;
+    printf("a %%d: %d\n", a);
+    printf("a %%i: %i\n", a);
+    printf("a %%c: %c\n", a + 32);
+    printf("a %%f: %f\n", (float)a);
+    return 0;
+}
---
+++ lec1/lec1_1st_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char strc[5] ="abcdesesese";
+    printf("strc %%c: %c\n", strc);
+    printf("strc %%d: %d\n", strc);
+    printf("strc %%s: %s\n", strc);
+
+    return 0;
+}
